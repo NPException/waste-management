4ed8aeb1d5312fdc58b30ce66c96785173587644
NPException <npexception42@gmail.com>
2020-04-26 11:29:22 +0200

    make block and item initialization easier

--- src/main/java/npe/mc/wastemanagement/common/ModBlocks.java
+++ src/main/java/npe/mc/wastemanagement/common/ModBlocks.java
@@ -1,36 +1,24 @@
 package npe.mc.wastemanagement.common;
 
-import static npe.mc.wastemanagement.util.Delay.delay;
-
-import java.util.Arrays;
-import java.util.List;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 import npe.mc.wastemanagement.common.block.BlockBase;
-import npe.mc.wastemanagement.util.Delay;
+import npe.mc.wastemanagement.util.ModUtils;
 
-@SuppressWarnings("StaticNonFinalField")
 public final class ModBlocks {
 	// add blocks here
-	public static BlockBase singularityUnit;
-
-
-	private static final Delay<List<BlockBase>> blocks = delay(() -> Arrays.asList(
-			  // instantiate blocks here (put comma at the start of line to minimize git changes)
-			  singularityUnit = new BlockBase(Material.ROCK, "singularity_unit")
-	));
+	public static final BlockBase singularityUnit = new BlockBase(Material.ROCK, "singularity_unit");
 
 
 	public static void registerBlocks(IForgeRegistry<Block> registry) {
-		blocks.get().forEach(registry::register);
+		ModUtils.publicStaticValues(ModBlocks.class, BlockBase.class)
+				  .forEach(registry::register);
 	}
 
 	public static void registerItemBlocks(IForgeRegistry<Item> registry) {
-		blocks.get()
-				  .stream()
+		ModUtils.publicStaticValues(ModBlocks.class, BlockBase.class)
 				  .map(BlockBase::createItemBlock)
 				  .forEach(registry::register);
 	}
--- src/main/java/npe/mc/wastemanagement/common/ModItems.java
+++ src/main/java/npe/mc/wastemanagement/common/ModItems.java
@@ -3,17 +3,16 @@ package npe.mc.wastemanagement.common;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 import npe.mc.wastemanagement.common.item.ItemBase;
+import npe.mc.wastemanagement.util.ModUtils;
 
 @SuppressWarnings("StaticNonFinalField")
 public final class ModItems {
 	// add items here
-	public static ItemBase singularityAnalyzer;
+	public static ItemBase singularityAnalyzer = new ItemBase("singularity_analyzer");
 
 
 	public static void registerItems(IForgeRegistry<Item> registry) {
-		registry.registerAll(
-				  // instantiate items here (put comma at the start of line to minimize git changes)
-				  singularityAnalyzer = new ItemBase("singularity_analyzer")
-		);
+		ModUtils.publicStaticValues(ModItems.class, Item.class)
+				  .forEach(registry::register);
 	}
 }
