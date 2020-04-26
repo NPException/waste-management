815be12246efcd4414fb8c46a2e334a8c66c45c8
NPException <npexception42@gmail.com>
2020-04-26 17:13:57 +0200

    add McJty tutorial TileEntity

--- src/main/java/npe/mc/wastemanagement/common/ModBlocks.java
+++ src/main/java/npe/mc/wastemanagement/common/ModBlocks.java
@@ -1,13 +1,22 @@
 package npe.mc.wastemanagement.common;
 
+import java.util.Arrays;
+import java.util.List;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.registries.IForgeRegistry;
+import npe.mc.wastemanagement.WasteManagement;
 import npe.mc.wastemanagement.common.block.BlockBase;
+import npe.mc.wastemanagement.common.block.DataBlock;
+import npe.mc.wastemanagement.common.tileentity.DataTileEntity;
 import npe.mc.wastemanagement.util.ModUtils;
 
 public final class ModBlocks {
@@ -17,22 +26,40 @@ public final class ModBlocks {
 
 	public static final BlockBase singularityUnit = new BlockBase(Material.ROCK, "singularity_unit");
 
+	// tutorial blocks
+	public static final DataBlock dataBlock = new DataBlock();
+
+
+	///////////////////
+	// TILE ENTITIES //
+	///////////////////
+
+	private static final List<Class<? extends TileEntity>> tileClasses = Arrays.asList(
+			  DataTileEntity.class
+	);
+
 
 	/////////////
 	// METHODS //
 	/////////////
 
+	private static void registerTileClass(Class<? extends TileEntity> tileClass) {
+		GameRegistry.registerTileEntity(tileClass,
+				  new ResourceLocation(WasteManagement.MOD_ID,
+							 "tile_" + tileClass.getSimpleName().toLowerCase()));
+	}
+
 	public static void registerBlocks(IForgeRegistry<Block> registry) {
 		ModUtils.publicStaticValues(ModBlocks.class, BlockBase.class)
 				  .forEach(registry::register);
+		tileClasses.forEach(ModBlocks::registerTileClass);
 	}
 
 	public static void registerItemBlocks(IForgeRegistry<Item> registry) {
-		//noinspection ConstantConditions
-		ModUtils.publicStaticValues(ModBlocks.class, BlockBase.class)
-				  .stream()
-				  .map(block -> new ItemBlock(block).setRegistryName(block.getRegistryName()))
-				  .forEach(registry::register);
+		for (BlockBase block : ModUtils.publicStaticValues(ModBlocks.class, BlockBase.class)) {
+			//noinspection ConstantConditions
+			registry.register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
+		}
 	}
 
 	@SideOnly(Side.CLIENT)
